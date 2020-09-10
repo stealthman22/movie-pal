6aff6d41ed36d387138fab1134dce810100e2773
stealthman22 <tdotonline@gmail.com>
2020-09-10 16:16:15 +0000

    updating app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const App = () => <div>Start Here!</div>;
+//  user defined components
+import Header from './elements/Header'
+import Home from './Home';
+
+
+const App = () => (
+<div>
+    <Header /> Start Here!
+    <Home />
+</div>);
+
 
 export default App;
+
+
+
