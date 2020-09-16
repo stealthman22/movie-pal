c5642312097af49ff1067d63ec5f847015ddcf4b
stealthman22 <tdotonline@gmail.com>
2020-09-16 11:14:09 +0000

    done with load more button, lots of short circuiting

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import {
 	API_URL,
 	API_KEY,
-	API_BASE_URL,
 	POSTER_SIZE,
 	BACKDROP_SIZE,
 	IMAGE_BASE_URL,
@@ -36,6 +35,19 @@ const Home = () => {
 	] = useHomeFetch();
 	// Let's create a search box state
 	const { searchTerm, setSearchTerm } = useState("");
+
+	const loadMoreMovies = () => {
+		const searchEndPoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage +
+			1}`;
+		const popularEndPoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage +
+			1}`;
+
+		const endpoint =
+			searchTerm ? searchEndPoint :
+			popularEndPoint;
+
+		fetchMovies(endpoint);
+	};
 
 	if (error) return <div>Something went wrong...</div>;
 	// Nothing shows on first render so prevent crash by:
@@ -74,9 +86,15 @@ const Home = () => {
 					/>
 				))}
 			</Grid>
-			<MovieThumb />
-			<Spinner />
-			<LoadMoreBtn />
+			{/* this is a short circuit, same as loading ? <Spinner /> :
+				"" */}
+			{loading && <Spinner />}
+			{/* and yet another shortcircuit
+				restrains loadmore btn from showing when we are loading, and doesn't 
+				show when we are on last page, after grabbing all movies
+			 */}
+			{currentPage < totalPages &&
+			!loading && <LoadMoreBtn text="Load More" callback={loadMoreMovies} />}
 		</React.Fragment>
 	);
 };
--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -11,6 +11,11 @@ export const useHomeFetch = () => {
 		setError(false);
 		setLoading(true);
 
+		// this would help us choose endpoint, by searching through the string
+		// search is a string method returns -1 if it doesn't find query
+
+		const isLoadMore = endpoint.search("page");
+
 		// try catch block
 		// Await the data, then await again to first parse it into json
 		try {
@@ -19,7 +24,10 @@ export const useHomeFetch = () => {
 			setState((prev) => ({
 				...prev,
 				// The returned movies
-				movies: [ ...result.results ],
+				movies:
+
+						isLoadMore !== -1 ? [ ...prev.movies, ...result.results ] :
+						[ ...result.results ],
 				// Short circuiting
 				heroImage: prev.heroImage || result.results[0],
 				currentPage: result.page,
@@ -42,3 +50,7 @@ export const useHomeFetch = () => {
 
 	return [ { state, loading, error }, fetchMovies ];
 };
+
+// In the fetch movies fucntion, after building isloadmore method, next we have to restructure state
+//  if we load more movies (), we want to append the new movies, and keep the old movies in the state
+// else we want to wipe out the old movies and display the new one
